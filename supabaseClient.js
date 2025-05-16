e8a3f7dabfe0fbd43d4ece8602647929b1adca16
long.phan <dragons3232@gmail.com>
2025-05-16 13:27:28 +0700

    Update supabase URL and API key to new MonacoCloud supabase

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,8 @@
 // supabaseClient.js
 
 // Replace with your Supabase project URL and Anon Key
-const SUPABASE_URL = 'https://cosygcghxkeoygncurtl.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvc3lnY2doeGtlb3lnbmN1cnRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA3NDkwODksImV4cCI6MjAzNjMyNTA4OX0.3Ldaabc1SJFppG7B3PHtJLf_bA9BoalUSEd2qeMMJY8';
+const SUPABASE_URL = 'https://bkmc1.skeal.co:4443';
+const SUPABASE_ANON_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJza2VhbC5jbyIsImlhdCI6MTc0NzE5MzgwMCwiZXhwIjoxODQxODg4MjAwLCJhdWQiOiJwb3J0YWwuc2tlYWwuY28iLCJzdWIiOiJtZWFsIHJlY29yZGluZyBhcHAiLCJyb2xlIjoiYW5vbiJ9.i-W7uS4APv_FYA4yR29a70r1LzCt0J84u5kjWGQiefQ';
 
 // Initialize Supabase client
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
